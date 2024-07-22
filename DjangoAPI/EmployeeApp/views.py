93c7225b39f00dd08e03a36b96e7976ea9b16191
PatrickNgare <patrickngare@gmail.com>
2024-07-22 14:46:07 +0300

    Added request views

--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+from .models import Departments,Employees
 
-# Create your views here.
+from .serializers import DepartmentSerializer,EmployeeSerializer
+
+@csrf_exempt
+def deparmentApi(request,id=0):
+
+    if request.method == 'GET':
+        
+
+
+
+
+
+
